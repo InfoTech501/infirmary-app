1cdaee64b03f607c385c7bbc21f36753fc1337b4
jakeeviado <jakematthewviado204@gmail.com>
2025-12-15 16:17:56 +0800

    linting configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,16 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
-const importPlugin = require('eslint-plugin-import');
 
 module.exports = defineConfig([
     expoConfig,
     {
-        plugins: {
-            import: importPlugin,
-        },
         settings: {
             'import/resolver': {
                 typescript: {
                     project: './tsconfig.json',
-                    alwaysTryTypes: true,
                 },
             },
         },
-    },
-    require('eslint-config-prettier'),
-    {
-        ignores: ['dist/*'],
     },
 ]);
